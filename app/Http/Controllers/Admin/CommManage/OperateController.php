84b91990231e09424dfe9d7314486d3493ede400
isCHEN <2911014925@qq.com>
2020-09-30 14:04:30 +0800

    陈秋香

---
+++ app/Http/Controllers/Admin/CommManage/OperateController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Admin\commManage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB;
+use App\Models\Login;
+use App\Models\UserInformation;
+class OperateController extends Controller
+{
+    public function insertMember(request $request){
+        $login_id=$request['user_id'];
+        $password=$request['user_password'];
+        $password=bcrypt($password);
+        $result=Login::insertMember($login_id,$password);
+        $res=UserInformation::insertInfromation($login_id);
+        // if($result){
+        //     return json_success("插入成员成功",null,200);
+        // }else{
+        //     return json_fail("插入成员失败",null,100);
+        // }
+        return $result;
+        return $res;
+    }
+     public function selectAll(){
+        $data = Login::selectAll();
+        if(count($data)){
+            return json_success("成功",$data,200);
+        }else{
+            return json_fail("失败",null,100);
+        }
+    }
+    public function showMember(){
+        $data=UserInformation::showMember();
+        // dd($data);
+        if(count((array)$data)>0){
+
+            return json_success("展示成员成功",$data,200);
+        }else{
+            return json_fail("展示成员失败",null,100);
+        }
+
+    }
+    public function queryMember(request $request){
+    $nichen=$request['nichen'];
+    $information_id=$request['user_id'];
+    $result=UserInformation::queryMember($nichen,$information_id);
+    if(count((array)$result)>0){
+        return json_success("查询数据成功",$result,200);
+    }else{
+        return json_fail("查询数据失败",null,100);
+    }
+  }
+
+    public function modifyMember(request $request){
+        $login_id=$request['user_id'];
+        $login_status=$request['user_status'];
+        $result=Login::modifyMember($login_id,$login_status);
+        return $result?
+        json_success("修改状态成功",$result,200):
+        json_fail("修改状态失败",null,100);
+    }
+}
--- app/Models/UserInformation.php
+++ app/Models/UserInformation.php
@@ -9,4 +9,50 @@ class UserInformation extends Model
     protected $table = "user_information";
     public $timestamps = true;
     protected $primaryKey = 'id';
+    protected $fillable = ['information_id'];
+
+    public static function showMember(){
+        $data =  self::join('login','information_id','login_id')
+            ->select('information_id','nichen','name','sex','login_date','login_status')
+            ->get();
+        
+         return $data;
+
+    }
+    public static function queryMember($nichen,$information_id){
+        // dd($information_id);
+        // dd($nichen);
+        if($nichen!=null){
+        $data=self::join('login','information_id','login_id')
+            ->select('information_id','nichen','name','sex','login_date')
+            ->where('nichen',$nichen)
+            ->get();
+            return $data;
+        }
+        else
+        {
+            $data=self::join('login','information_id','login_id')
+            ->select('information_id','nichen','name','sex','login_date')
+            ->where('information_id',$information_id)
+            ->get();
+            return $data;
+        }
+      
+    }
+    public static function insertInfromation($login_id)
+    {
+        try{
+            $res=self::insert(
+                [
+                     'information_id'=>$login_id,
+                ]
+                );
+         return json_success(200,"插入成功",null);
+        }
+        catch(\Exception $e){
+           logError("插入失败",[$e -> getMessage()]);
+            return json_fail(100,"插入失败",null);
+        }
+    }
 }
+
